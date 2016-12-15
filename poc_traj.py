86141f0cb77db7c9b8a6cb565d5032337c77ca55
thomasdelrue <thomasdelrue@gmail.com>
2016-12-15 13:01:11 +0100

    Add files via upload

--- poc_traj.py
+++ poc_traj.py
@@ -3,7 +3,7 @@ import random
 import pygame
 import pygame.gfxdraw
 from pygame.locals import *
-from sys import exit
+from sys import *
 
 from vector2 import Vector2
 
@@ -16,6 +16,8 @@ pygame.draw.line(img, (255, 0, 0), (10, 10), (10, 0))'''
 
 
 rotation = 0.0
+
+#setrecursionlimit(1500)
 
 def bezier1(p0, p1):
     # calculate 20 points...
@@ -61,19 +63,54 @@ def bezier3(p0, p1, p2, p3, t):
 	y = (1 - t) ** 3 * p0.y + 3 * (1 - t) ** 2 * t * p1.y + 3 * (1 - t) * t ** 2 * p2.y + t ** 3 * p3.y
 	return Vector2(int(x), int(y))
 	
-
-def findPoints(t0, t1, insertionIndex, pointList):
-	pass
+def dot(v1, v2):
+	return v1.x * v2.x + v1.y * v2.y
+	
+	
+def findDrawingPoints(t0, t1, insertionIndex, pointList):
+	tMid = (t0 + t1) / 2
+	lp0 = bezier3(p0, p1, p2, p3, 0)
+	lp1 = bezier3(p0, p1, p2, p3, 1)
+	
+	if (lp0 - lp1).get_magnitude() < 100:
+		return 0
+	
+	pMid = bezier3(p0, p1, p2, p3, tMid)
+	leftDirection = (lp0 - pMid) 
+	leftDirection.normalize()
+	rightDirection = (lp1 - pMid) 
+	rightDirection.normalize()
+	
+	print('3v', lp0, lp1, pMid)
+	print('l', leftDirection)
+	print('r', rightDirection)
+	
+	print(dot(leftDirection, rightDirection))
+	dotP = dot(leftDirection, rightDirection)
+	if dotP > -0.99 and dotP != 0.0:
+		pointsAdded = 0
+		
+		pointsAdded += findDrawingPoints(t0, tMid, insertionIndex, pointList)
+		
+		pointList.insert(insertionIndex + pointsAdded, pMid)
+		pointsAdded += findDrawingPoints(tMid, t1, insertionIndex + pointsAdded, pointList)
+		
+		return pointsAdded
+		
+	return 0
 	
 	
 def findPoints():
 	pointList = []
 	lp0 = bezier3(p0, p1, p2, p3, 0)
 	lp1 = bezier3(p0, p1, p2, p3, 1)
+	pointList.append(lp0)
+	pointList.append(lp1)
 	
-	int pointsAdded = findPoints(0, 1, 1, pointList)
+	pointsAdded = findDrawingPoints(0, 1, 1, pointList)
 	
 	assert(pointsAdded + 2 == len(pointList))
+	return pointList
 	
 	
 pygame.init()
@@ -86,10 +123,10 @@ img = sprite_map.subsurface((20, 100, 20, 20))'''
 clock = pygame.time.Clock()
 
 p0 = Vector2(100, 100)
-p1 = Vector2(400, 500)
+p1 = Vector2(200, 100)
 
-p2 = Vector2(400, 100)
-p3 = Vector2(100, 500)
+p2 = Vector2(100, 200)
+p3 = Vector2(100, 200)
 
 RED = (255, 0, 0)
 
@@ -102,9 +139,11 @@ print(points)
 
 points.extend(bezier2(p0, p2, p1))'''
 
-points.append(bezier3(p0, p2, p1, p3, 0))
+'''points.append(bezier3(p0, p2, p1, p3, 0))
 points.append(bezier3(p0, p2, p1, p3, 1))
-points.append(bezier3(p0, p2, p1, p3, .5))
+points.append(bezier3(p0, p2, p1, p3, .5))'''
+
+points = findPoints()
 print(points)
 
 '''vo = Vector2(0, -10)
